14e23098ab967f1b64215541f59f3d21e0d0f1b4
Janae Quinones <janaemassa@gmail.com>
2021-06-13 01:36:04 -0400

    create login action

---
+++ foodiefriends/src/actions/loggedAction.js
@@ -0,0 +1,12 @@
+export const fetchLogin = (userData) => dispatch => {
+    console.log('action called')
+    fetch('http://localhost:3000/api/v1/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+        .then(r => r.json())
+        .then(data => console.log(data))
+}
