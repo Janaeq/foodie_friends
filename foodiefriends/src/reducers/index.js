1b1145724025830896a32a81c01e0110f5d8fcb4
Janae Quinones <janaemassa@gmail.com>
2021-06-13 01:35:35 -0400

    change isLogged to loggedReducer

--- foodiefriends/src/reducers/index.js
+++ foodiefriends/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import loggedReducer from './isLogged'
+import loggedReducer from './loggedReducer'
 
 const allReducers = combineReducers({
     isLogged: loggedReducer
