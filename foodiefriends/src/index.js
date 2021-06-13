806cc53bb3be924c39b90bdbcd656a98cc7c4b23
Janae Quinones <janaemassa@gmail.com>
2021-06-12 21:12:56 -0400

    implement react router and redux provider

--- foodiefriends/src/index.js
+++ foodiefriends/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+// REDUX IMPORTS
+import { Provider } from 'react-redux'
+
+// STORE
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
