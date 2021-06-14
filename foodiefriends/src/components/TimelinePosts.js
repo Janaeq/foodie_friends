4a34ef049efcddd542d291685a7db9a95456d328
Janae Quinones <janaemassa@gmail.com>
2021-06-14 07:23:50 -0400

    display posts on timeline

--- foodiefriends/src/components/TimelinePosts.js
+++ foodiefriends/src/components/TimelinePosts.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 
 class TimelinePosts extends Component {
-    state = {
-        allPosts: []
-    }
 
     render() {
+        console.log(this.props.posts)
+        const posts = this.props.posts.map(post => (
+            <div key={post.id} className='post-card'>
+                <div className='post-header'>
+                    {/* where is the user profile pic? */}
+                    <h4>{post.name}</h4>
+                    <p>by - {post.user.username}</p>
+                </div>
+                <div className='post-img'>
+                    <p>{post.img}</p>
+                </div>
+                <div className='actions'>
+                    <button className='like'>Like</button>
+                    <button className='ingredients'>Ingredients</button>
+                    <button className='directions'>Directions</button>
+                    <p>ingredients or directions</p> {/* implement toggler here */}
+                </div>
+            </div>
+        ))
         return (
-            <p>Iterate over all posts in db and display here</p>
+            <div>
+                { posts }
+            </div>
         )
     }
 }
--- foodiefriends/src/containers/Home.js
+++ foodiefriends/src/containers/Home.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+
 import Nav from '../components/Nav'
 import TimelinePosts from '../components/TimelinePosts'
+
+import fetchPosts from '../actions/timelineAction'
 class Home extends Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
     render() {
-        console.log(this.props)
+        // console.log(this.props)
         return (
             <div>
                 <Nav />,
-                <TimelinePosts />
+                <TimelinePosts posts={this.props.posts} />
             </div>
         )
     }
 }
 
-export default Home
+const mapStateToProps = state => {
+    return {
+        posts: state.posts.posts,
+        post: state.posts.post
+    }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(Home)
