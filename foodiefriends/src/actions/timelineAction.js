e4204645d3bbc21e2648402453e13fd9378943c3
Janae Quinones <janaemassa@gmail.com>
2021-06-14 06:55:04 -0400

    add timeline action

---
+++ foodiefriends/src/actions/timelineAction.js
@@ -0,0 +1,15 @@
+export const fetchPosts = () => {
+    return dispatch => {
+        dispatch({ type: 'FETCH_POSTS' })
+        return fetch('http://localhost:3000/api/v1/posts')
+        .then(r => r.json())
+        .then(posts => dispatch({
+            type: 'FETCH_POSTS',
+            payload: posts
+        }))
+    }
+}
+
+
+
+export default fetchPosts
