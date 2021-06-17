493eb4136b1c4f66ffba4b9538ab3fb70cc7d720
Janae Quinones <janaemassa@gmail.com>
2021-06-17 15:45:16 -0400

    add user posts to serializer

--- foodiefriends-api/app/serializers/user_serializer.rb
+++ foodiefriends-api/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :bio, :img, :first_name, :last_name
+  attributes :id, :username, :bio, :img, :first_name, :last_name, :posts
 end
