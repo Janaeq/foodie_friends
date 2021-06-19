fa537c8e127209798df00bdf3bb96548134397ba
Janae Quinones <janaemassa@gmail.com>
2021-06-19 14:11:55 -0400

    remove extra user attributes from state

--- foodiefriends/src/components/ProfileHeader.js
+++ foodiefriends/src/components/ProfileHeader.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 class ProfileHeader extends Component {
     render() {
+        console.log(this.props)
         const user = this.props.user
         return (
             <div className='header'>
                 <div className='user-id'>
-                    <img src={user.img} alt='user' />
-                    <p>{user.username}</p>
-                    <Link to='/edit_profile'>
-                    <button className='edit-profile'>Edit Profile</button>
-                    </Link>
-                </div>
-                <div className='bio'>
-                    <p>{user.first_name} {user.last_name}</p>
-                    <p>{user.bio}</p>
+                    <p>Posts by {user.username}</p>
                 </div>
                 <div className='stats'>
                     <p>{user.posts.length} posts</p>
