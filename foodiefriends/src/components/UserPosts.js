a06a94bc132bde0fb70dcdd9cebaacd17994cf1a
Janae Quinones <janaemassa@gmail.com>
2021-06-19 19:08:55 -0400

    add user posts timeline

--- foodiefriends/src/components/UserPosts.js
+++ foodiefriends/src/components/UserPosts.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 
 class UserPosts extends Component {
     render() {
+        console.log(this.props.user.posts[0])
         const userPosts = this.props.user.posts.map(post => (
-            <div key={post.id} className='grid-img'>
+            <div key={post.id} className='grid-img' onClick={this.onClick} >
+                <p>{post.name}</p>
                 <p>{post.img}</p>
+                <p>{post.directions}</p>
+                <p>{post.ingredients}</p>
             </div>
         ))
         return (
