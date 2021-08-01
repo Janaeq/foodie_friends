a8c31eea78934777b4b79c61d2f7d6e87a3e63a7
Janae Quinones <janaemassa@gmail.com>
2021-08-01 17:20:29 -0400

    show posts in chronological order

--- foodiefriends/src/components/UserPosts.js
+++ foodiefriends/src/components/UserPosts.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 class UserPosts extends Component {
     render() {
 
-        const userPosts = this.props.user.posts.map(post => (
+        const userPosts = this.props.user.posts.reverse().map(post => (
             <div key={post.id} className='grid-img' onClick={this.onClick} >
                 <Link to={{pathname: `/posts/${post.id}`}}>
                     <img
