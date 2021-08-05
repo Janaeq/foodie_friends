1b59aa76db5481858bcbc35c1ed27d033a903e9d
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-04 22:01:06 -0400

    create likes table

--- foodiefriends-api/db/schema.rb
+++ foodiefriends-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_201638) do
+ActiveRecord::Schema.define(version: 2021_08_05_015256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2021_08_03_201638) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "measurement"
     t.index ["post_id"], name: "index_ingredients_on_post_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "like_count"
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
