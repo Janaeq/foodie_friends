cbe925ab6f461a0937678b9749264925cf2e0bf8
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-10 16:38:40 -0400

    add ingredients to post

--- foodiefriends/src/components/Ingredients.js
+++ foodiefriends/src/components/Ingredients.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react'
 
 class Ingredients extends Component {
     render() {
+        console.log(this.props.ingredients)
+        const ingredientsList = this.props.ingredients.map(ingr => (
+            <div key={ingr.id} className='ingredient'>
+                <li>{ingr.measurement} {ingr.ingredient}</li>
+                <br></br>
+            </div>
+    ))
         return (
-            <div>{this.props.post.ingredients}</div>
+            <div>{ingredientsList}</div>
         )
     }
 }
