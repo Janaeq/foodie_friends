037621bde32d257e432d5b595985065ba3372089
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-10 16:13:03 -0400

    add directions component

--- foodiefriends/src/components/Directions.js
+++ foodiefriends/src/components/Directions.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 class Directions extends Component {
     render() {
+        const directionsList = this.props.directions.map((step, i) => (
+            <div key={step.id} className='direction'>
+                <strong>Step {i + 1}:</strong> {step.direction}
+                <br></br><br></br>
+            </div>
+        ))
         return(
-            <div>{this.props.post.directions}</div>
+            <div>{directionsList}</div>
         )
     }
 }
