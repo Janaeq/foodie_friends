437d617bae5d52d48f18b1de018bafd5fd89a728
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-24 16:34:41 -0400

    notes

--- foodiefriends/src/reducers/postReducer.js
+++ foodiefriends/src/reducers/postReducer.js
@@ -1,10 +1,12 @@
 const postReducer = (state = { items: [], newItem: {}, item: {} }, action) => {
     switch(action.type) {
         case 'FETCH_POSTS':
+            // payload: array of all post hashes
             return { ...state, items: action.payload }
         case 'NEW_POST':
             return { ...state, newItem: action.payload }
         case 'FETCH_POST':
+            // payload: single post hash containing id, name, img, directions, ingredients, and user info
             return { ...state, item: action.payload }
         default:
             return state
