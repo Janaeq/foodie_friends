413e63f32ec8305836a60e6c5cbca5d7336b7f7a
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-24 16:34:28 -0400

    notes

--- foodiefriends/src/reducers/loggedReducer.js
+++ foodiefriends/src/reducers/loggedReducer.js
@@ -2,6 +2,7 @@ const loggedReducer = (state = { currentUser: [] }, action) => {
   switch (action.type) {
     case 'LOGIN_USER':
       // return the currect state with the new data added 
+      // payload is a hash containing id and username
       return { ...state, currentUser: action.payload }
     case 'LOGGING_USER':
       return { ...state }
