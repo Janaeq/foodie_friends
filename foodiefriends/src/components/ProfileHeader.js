0968517e481eb4156c7e6a839840951ff2437de3
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-08-24 16:33:12 -0400

    notes

--- foodiefriends/src/components/ProfileHeader.js
+++ foodiefriends/src/components/ProfileHeader.js
@@ -10,14 +10,24 @@ class ProfileHeader extends Component {
         }
     }
     render() {
+        // props: user => id, posts, username
         const user = this.props.user
         return (
             <div className='header'>
+            {/* HEADER TO BE SECTIONED FROM */}
                 <div className='user-id'>
-                    <h1>Posts by {user.username}</h1>
+                    {/* INLINE - PHOTO ON LEFT, USERNAME ON RIGHT */}
+                    {/* <img src={user.photo}/> */}
+                    <h1>{user.username}</h1>
                 </div>
                 <div className='stats'>
+                    {/* INLINE */}
                     {this.postCount()}
+                    {/* user.followers.length */}
+                    {/* user.following.length */}
+                </div>
+                <div className='bio'>
+                    {/* user.bio */}
                 </div>
             </div>
         )
