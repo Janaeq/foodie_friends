a3c6a918f8c15dabbe9f977ee98d8606198112c2
Janae Quinones <62717289+Janaeq@users.noreply.github.com>
2021-09-09 10:19:29 -0400

    remove measurement column from posts

---
+++ foodiefriends-api/db/migrate/20210909141659_remove_measurement_from_ingredients.rb
@@ -0,0 +1,5 @@
+class RemoveMeasurementFromIngredients < ActiveRecord::Migration[6.1]
+  def change 
+    remove_column :ingredients, :measurement
+  end
+end
--- foodiefriends-api/db/schema.rb
+++ foodiefriends-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_015256) do
+ActiveRecord::Schema.define(version: 2021_09_09_141659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_015256) do
     t.bigint "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "measurement"
     t.index ["post_id"], name: "index_ingredients_on_post_id"
   end
 
